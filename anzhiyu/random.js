20d6a691820371821a4182c0e1ca7e225f68f102
firefly0916 <hermitally@gmail.com>
2024-11-09 23:45:57 +0800

    Site updated: 2024-11-09 23:45:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/09/hello-world/","2024/11/09/first-page/"];function toRandomPost(){
+var posts=["2024/11/09/first-page/","2024/11/09/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
